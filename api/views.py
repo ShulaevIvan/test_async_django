562b7658f6c175bc89c28ab6ae916c25d3318c8c
shulaev ivan <mail@ntdservice.ru>
2025-03-08 18:52:45 +0300

    tests

--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 import os
 import json
+import re
+from datetime import datetime
 from django.shortcuts import render
 from django.core.mail import EmailMessage
 from rest_framework.response import Response
@@ -9,6 +11,8 @@ import asyncio
 from asgiref.sync import sync_to_async
 from docxtpl import DocxTemplate
 
+from django.conf import settings
+
 from .models import TestModel
 
 # Create your views here.
@@ -16,29 +20,81 @@ from .models import TestModel
 class TestView(APIView):
 
     async def post(self, request):
-        data = json.loads(request.body)
-        send_data = {
-            'name': data.get('name'),
-            'phone': data.get('phone')
+        respone_data = {
+            "status": 'ok',
+            "description": '',
         }
-        user_data = await asyncio.create_task(create_user(send_data))
-        email = asyncio.create_task(sendEmail())
-        file = asyncio.create_task(create_docx_file(send_data))
-        result = await asyncio.gather(file, email)
-        print(user_data.name)
 
-        return Response({"result": {'status': 'ok', 'file': ''}})
+        try:
+            order_type = 'contract'
+            data = json.loads(request.body)
+            send_data = dict()
+            send_data = {
+                'name': data.get('name'),
+                'phone': data.get('phone'),
+                'client_email': ''
+            }
+            user_data = asyncio.create_task(create_user(send_data))
+            email_send_task = asyncio.create_task(sendEmail(send_data, order_type))
+            file_create_task = asyncio.create_task(create_docx_file(send_data))
+            result_status = await asyncio.gather(user_data, email_send_task, file_create_task)
 
-async def sendEmail():
-    msg_mail = EmailMessage(
-        f"test", 
-        f"""test
-        """,
-        'test@smprof.ru', [f"pro@cosmtech.ru"]
-    )
-    msg_mail.content_subtype = "html"
-    msg_mail.send()
+            if not email_send_task.result():
+                return Response({"result": {'status': respone_data['status'], 'description': 'err to send email'}})
 
+
+            return Response({"result": {'status': respone_data['status'], 'description': 'ok'}})
+
+        except Exception as err:
+            respone_data['status'] = 'err'
+            respone_data['description']= 'program err'
+
+            return Response({"result": {'status': respone_data['status']}})
+       
+async def sendEmail(email_data, email_type='order', to_email=None):
+
+    email_pattern = r'\S*\@\S*\.\w{2,10}$'
+    feedback_email = email_data.get('client_email')
+    order_email = settings.EMAIL_HOST_USER
+    send_feedback_to_client = True
+
+    if not re.match(email_pattern, order_email) and not re.match(email_pattern, feedback_email):
+        return False
+    elif not re.match(email_pattern, feedback_email) or not feedback_email:
+        send_feedback_to_client = False
+
+    try:
+        msg_mail = EmailMessage(
+            f"test", 
+            f"""
+                test
+            """,
+            f'{settings.EMAIL_HOST_USER}', [f"{settings.EMAIL_ORDER_ADDRESS}"]
+        )
+        msg_mail.content_subtype = "html"
+        msg_mail.send()
+
+        if send_feedback_to_client:
+            try:
+                client_mail = EmailMessage(
+                f"test", 
+                f"""
+                    test
+                """,
+                f'{settings.EMAIL_HOST_USER}', [f"{feedback_email}"]
+                )
+                client_mail.content_subtype = "html"
+                client_mail.send()
+
+            except Exception as err:
+                await write_err_email_log(settings.EMAIL_HOST_USER, feedback_email, err)
+        
+        return True
+    
+    except Exception as err:
+        await write_err_email_log(settings.EMAIL_HOST_USER, settings.EMAIL_ORDER_ADDRESS, err)
+
+        return False
 
 async def create_docx_file(data):
     doc_tmp = DocxTemplate(f'{os.getcwd()}/files/test.docx')
@@ -58,3 +114,13 @@ async def create_user(data):
         phone=data.get('phone')
     )
     return user
+
+async def write_err_email_log(from_email, to_email, err_code):
+    if not os.path.exists(settings.ERR_LOG_FOLDER):
+        os.mkdir(settings.ERR_LOG_FOLDER)
+
+    err_date = re.sub(r'.\d+$', '', str(datetime.now()))
+    err_description = f"date: {err_date} from: {from_email} to: {to_email} err: {err_code}"
+
+    with open(f'{settings.ERR_LOG_FOLDER}/email_send_err.txt', 'a+') as file:
+        file.write(f'{err_description} \n')
--- django_async/settings.py
+++ django_async/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -30,9 +30,14 @@ CORS_ALLOW_METHODS = ('GET','POST','PUT','PATCH','DELETE','OPTIONS',)
 
 MAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.timeweb.ru'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
 SECURE_CROSS_ORIGIN_OPENER_POLICY=None
+
+EMAIL_ORDER_ADDRESS = 'pro@cosmtech.ru'
+ERR_LOG_FOLDER = f'{os.getcwd()}/err_logs'
 
 
 # Application definition
